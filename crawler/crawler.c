c6a752e239dbc328ea6024bedb9151db77748d54
Eren Berke Saglam <erenberke12@gmail.com>
2023-02-07 11:03:56 -0500

    crawler step 2

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -11,10 +11,50 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "webpage.h"
+
+void url_scanner(webpage_t* my_page) {
+
+	int pos = 0;
+
+	char* next_url;
+
+	while ((pos = webpage_getNextURL(my_page, pos, &next_url)) > 0) {
+
+		if (IsInternalURL(next_url)) {
+			printf("%s, internal\n", next_url);
+		}
+
+		else {
+			printf("%s, external\n", next_url);
+		}
+
+		free(next_url);
+	}
+}
 
 int main(void) {
 
-	printf("hello");
+	char* my_url = "https://thayer.github.io/engs50/";
+
+	webpage_t* my_page = webpage_new(my_url, 0, NULL);
+
+	if(webpage_fetch(my_page)) {
+
+		webpage_getHTML(my_page);
+
+	}
+
+	else {
+		printf("error in fetching html\n");
+		exit(EXIT_FAILURE);
+	}
+
+	url_scanner(my_page);
+
+	webpage_delete(my_page);
+
+	exit(EXIT_SUCCESS);
 
 }
 
