279310561aa8088d1f08c8be491708bcab486a95
Eren Berke Saglam <erenberke12@gmail.com>
2023-02-07 20:49:25 -0500

    slightly fixed crawler

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -17,6 +17,11 @@
 #include <stdbool.h>
 #include <string.h>
 
+void printUrl(void* Url){
+	char* UrlC= (char*) Url;
+	printf("%s\n", UrlC);
+}
+
 bool url_matcher(void* url1, const void* url2) {
 	char* urlstr1 = (char*) url1;
 	char* urlstr2 = (char*) url2;
@@ -42,33 +47,30 @@ queue_t* url_scanner(webpage_t* my_page) {
 	
 	while ((pos = webpage_getNextURL(my_page, pos, &next_url)) > 0) {
 
-		if (hsearch(url_table, url_matcher, next_url, strlen(next_url)) == NULL) {
+		if (hsearch(url_table, url_matcher, next_url, sizeof(next_url)) == NULL) {
 
-			hput(url_table, next_url, next_url, strlen(next_url));
+			char* copy = malloc(strlen(next_url) +1);
+
+			strcpy(copy, next_url);
+
+			hput(url_table, next_url, next_url, sizeof(next_url));
 
 			if (IsInternalURL(next_url)) {
 			
-				qput(internalLinks, next_url);
+				qput(internalLinks, copy);
 			//			printf("%s, internal\n", next_url);
 			}
 
 		//		else {
 			//			printf("%s, external\n", next_url);
 		//		}
-
-		//free(next_url);
+			
 		}
 	}
-	
-	free(next_url);
+
 	hclose(url_table);
 		
 	return internalLinks;
-}
-
-void printUrl(void* Url){
-	char* UrlC= (char*) Url;
-	printf("%s\n", UrlC);
 }
 
 
@@ -92,7 +94,7 @@ int main(void) {
 	queue_t* links = url_scanner(my_page);
 	qapply(links, printUrl);
 	webpage_delete(my_page);
-	
+
 	qclose(links);
 	
 	exit(EXIT_SUCCESS);
