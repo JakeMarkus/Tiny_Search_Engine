744531df470706e4a2ba3f5e38baf259d78f80e7
Eren Berke Saglam <erenberke12@gmail.com>
2023-02-14 10:03:53 -0500

    new files added

---
+++ tests/test1.c
@@ -0,0 +1,44 @@
+/* test1.c --- 
+ * 
+ * 
+ * Author: Eren Berke Saglam
+ * Created: Mon Feb 13 19:01:09 2023 (-0500)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+
+#include "webpage.h"
+#include <stdio.h>
+#include "pageio.h"
+#include <string.h>
+
+int compare_webpage(webpage_t* file1, webpage_t* file2) {
+	if (webpage_getDepth(file1) == webpage_getDepth(file2)) {
+		if (strcmp(webpage_getHTML(file1), webpage_getHTML(file2)) == 0) {
+			if (strcmp(webpage_getURL(file1), webpage_getURL(file2)) == 0) {
+				return 0;
+			}
+		}
+	}
+
+	return 1;
+}
+
+int main(void) {
+
+	printf("test");
+	
+	webpage_t* my_page = pageload(1, "../pages/");
+
+	pagesave(my_page, 10000000, "../pages/");
+
+	webpage_t* my_page_2 = pageload(10000000, "../pages/");
+
+	int num = compare_webpage(my_page, my_page_2);
+
+  printf("%d", num);
+
+}	
+	
---
+++ utils/pageio.c
@@ -0,0 +1,160 @@
+/* pageio.c --- 
+ * 
+ * 
+ * Author: Eren Berke Saglam
+ * Created: Fri Feb 10 18:50:49 2023 (-0500)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include "webpage.h"
+
+
+int32_t pagesave(webpage_t *pagep, int id, char *dirname)                                                                                     
+{                                                                                                                                             
+  FILE * fp;                                                                                                                                  
+                                                                                                                                              
+  char id_str [250]; //id/10 + 1                                                                                                              
+                                                                                                                                              
+  char path[250] = "";                                                                                                                        
+                                                                                                                                              
+  sprintf(id_str, "%d", id);                                                                                                                  
+                                                                                                                                              
+  strcat(path, dirname);                                                                                                                      
+  strcat(path, id_str);                                                                                                                       
+                                                                                                                                              
+                                                                                                                                              
+  if(access(dirname, W_OK)== -1){                                                                                                             
+    printf("Cannot access %s to save pages!\n", dirname);                                                                                     
+    return 1;                                                                                                                                 
+    }                                                                                                                                         
+  fp = fopen(path, "w+");                                                                                                                     
+                                                                                                                                              
+  if (fp == NULL){                                                                                                                            
+      return 2;                                                                                                                               
+    }                                                                                                                                         
+                                                                                                                                              
+  char* html = webpage_getHTML(pagep);                                                                                                        
+                                                                                                                                              
+  char html_len[250];                                                                                                                         
+  sprintf(html_len, "%d", (int)webpage_getHTMLlen(pagep));                                                                                    
+                                                                                                                                              
+  char dephstr[250];                                                                                                                          
+  sprintf(dephstr, "%d", (int)webpage_getDepth(pagep));  
+	int err = fprintf(fp, "%s\n%s\n%s\n%s", webpage_getURL(pagep), dephstr,html_len, html );                                                   
+                                                                                                                                             
+  if (err < 0){                                                                                                                              
+    printf("Failed to write to file!\n");                                                                                                    
+    return 3;                                                                                                                                
+  }                                                                                                                                          
+                                                                                                                                             
+  fclose(fp);                                                                                                                                
+	
+  return 0;                                                                                                                                  
+}
+
+webpage_t* pageload(int id, char* dirnm) {
+
+
+  FILE* curr_file;
+	
+	char id_str [100] = "";
+
+	char path[100] = "";
+
+	sprintf(id_str, "%d", id);
+
+	//printf("inpageload/n");
+	
+	strcat(path, dirnm);
+	strcat(path, id_str);
+
+	if(access(dirnm, W_OK) == -1) {
+		printf("cannot access %s to save pages\n", dirnm);
+		return NULL;
+
+	}
+
+	//printf("\n%s\n", path);
+	
+	curr_file = fopen(path, "r");
+	
+	if(curr_file == NULL){
+		return NULL;
+		}
+
+	
+	char curr_line[1000] = "";
+
+	char url [300] = "";
+	char depth[300] = "";
+	char HTML[99999] = ""; 
+	
+
+
+	//printf("yee0\n");
+	
+	int num_line = 0;
+
+	//printf("yee");
+	fflush(stdout);
+
+	while(fgets(curr_line, 1000, curr_file)){
+
+		//	printf("hello\n");
+		fflush(stdout);
+		if(num_line == 0){
+			strcat(url, curr_line);
+		}
+		
+		else if(num_line == 1){
+			strcat(depth, curr_line);
+		}
+
+		else if(num_line == 2) {
+			num_line++;
+			continue;
+		}
+		
+		else{
+			strcat(HTML, curr_line);	
+		}
+		
+		num_line++;
+
+		//		printf("hry");
+	}
+	
+	int int_depth = atoi(depth);
+	
+	webpage_t* new_page = webpage_new(url, int_depth, HTML);
+
+	
+	
+	return new_page;
+}
+
+		
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
