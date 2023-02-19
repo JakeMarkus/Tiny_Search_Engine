c2af31c731089849e60e615811dbe43969528df2
JakeMarkus <jakemarkus3@gmail.com>
2023-02-19 16:54:31 -0500

    Broken file with progress, put here to debug.

--- querier/query.c
+++ querier/query.c
@@ -10,6 +10,53 @@
  */
 
 
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+static void cleanInput(char* input)
+{
+	char* block = (char*)malloc(100*sizeof(char));
+	strcpy(block, "");
+
+	char*[] output = (char*[])malloc(10000 * sizeof(char));
+
+	
+	for(int i = 0; i <= strlen(input); i++)
+		{
+			if((char)input[i] == ' ' || i == strlen(input))
+				{
+					if(strcmp(block, "") != 0)
+						{
+							char* cpy;
+							strcpy(cpy, block);
+							output[i] = cpy; 
+						}
+
+					free(block);
+					strcpy(block, "");
+				}
+			else
+				{
+					char letter = tolower(input[i]);
+					strncat(block, letter, 1);
+				}
+		}
+
+	return output; 
+}
 int main(void)
 {
+	char* currline;
+
+	char** currwords;
+	
+	while(true)
+		{
+			printf("> ");
+			scanf("%s", currline);
+			currwords = (char**)cleanInput(currline);
+			
+			
+			strcpy(currline, "");
+		}
 }
---
+++ querier/queury.c
@@ -0,0 +1,10 @@
+/* queury.c --- 
+ * 
+ * 
+ * Author: Jacob R. Markus
+ * Created: Sun Feb 19 16:52:53 2023 (-0500)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
