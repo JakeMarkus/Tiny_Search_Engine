9d773c47db0603702ec46b647e628d4e0e8bcad2
JakeMarkus <jakemarkus3@gmail.com>
2023-02-21 11:53:26 -0500

    Working query file. Has Valgrind leaks and no quit condition yet

--- querier/query.c
+++ querier/query.c
@@ -11,15 +11,35 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <stdbool.h>
-static void cleanInput(char* input)
+#include "indexio.h"
+#include <string.h>
+#define MAX_LINE 100
+
+bool isChar(char c)
+{
+	int n = (int)c;
+	
+	if ( n < 65  || n  > 122)
+		{
+			return false;
+		}
+	else if (n > 90 && n < 97 )
+		{
+			return false;
+		}
+	
+	return true;
+}
+
+static char* cleanInput(char* input)
 {
 	char* block = (char*)malloc(100*sizeof(char));
 	strcpy(block, "");
 
-	char*[] output = (char*[])malloc(10000 * sizeof(char));
-
+	char* output = (char*)malloc(10000 * sizeof(char));
 	
 	for(int i = 0; i <= strlen(input); i++)
 		{
@@ -27,18 +47,26 @@ static void cleanInput(char* input)
 				{
 					if(strcmp(block, "") != 0)
 						{
-							char* cpy;
-							strcpy(cpy, block);
-							output[i] = cpy; 
+							strcat(output, block);
+							
+							if(i!= strlen(input))
+								strcat(output, " ");
 						}
 
-					free(block);
+					//free(block);
 					strcpy(block, "");
 				}
 			else
 				{
-					char letter = tolower(input[i]);
-					strncat(block, letter, 1);
+					if(!isChar((char)input[i]) && input[i] != 10)
+						{
+							return NULL;
+						}
+					else
+						{
+							char letter = tolower(input[i]);
+							strncat(block, &letter, 1);
+						}
 				}
 		}
 
@@ -48,14 +76,32 @@ int main(void)
 {
 	char* currline;
 
-	char** currwords;
+	char user_input[MAX_LINE];
+	//strcpy(currline, "");
+	char* currwords;
 	
 	while(true)
 		{
 			printf("> ");
-			scanf("%s", currline);
-			currwords = (char**)cleanInput(currline);
+			if((currline = fgets(user_input, MAX_LINE, stdin)) == NULL)
+				{
+					continue; 
+				}
+	 
+			if((currwords = (char*)cleanInput(currline)) == NULL)
+				{
+					printf("Invalid Input!\n");
+					continue;
+				}
 			
+			//if(strcmp(currline, "q") == 0)
+				//{
+					//printf("yee");
+					//FREE STUFF
+					//exit(EXIT_SUCCESS);
+					//break;
+					//}
+			printf("Cleaned Input: %s", currwords);
 			
 			strcpy(currline, "");
 		}
